08caa1e9def75f23332ad66e8844b3a90ee38f7b
cuten <cutenara03@naver.com>
2018-04-25 16:08:06 +0900

    2018.04.25.1

--- app/src/main/java/com/example/cuten/android_sms/MainActivity.java
+++ app/src/main/java/com/example/cuten/android_sms/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.cuten.keganzipj;
+package com.example.cuten.android_sms;
 
 import android.Manifest;
 import android.content.DialogInterface;
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 //Git 확인
 
